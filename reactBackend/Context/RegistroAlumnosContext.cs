19349d3c90832e72dac857af79d615b61dfc9990
RBryan05 <rg0358032023@unab.edu.sv>
2025-02-27 08:55:01 -0600

    Guía 2: Uso de Entity Framework

---
+++ reactBackend/Context/RegistroAlumnosContext.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using reactBackend.Models;
+
+namespace reactBackend.Context;
+
+public partial class RegistroAlumnosContext : DbContext
+{
+    public RegistroAlumnosContext()
+    {
+    }
+
+    public RegistroAlumnosContext(DbContextOptions<RegistroAlumnosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Alumno> Alumnos { get; set; }
+
+    public virtual DbSet<Asignatura> Asignaturas { get; set; }
+
+    public virtual DbSet<Calificacion> Calificacions { get; set; }
+
+    public virtual DbSet<Matricula> Matriculas { get; set; }
+
+    public virtual DbSet<Profesor> Profesors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-03PS3SB\\ADB;Initial Catalog=RegistroAlumnos;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Alumno>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Alumnos__3213E83FAAA79ABB");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Dni)
+                .HasMaxLength(8)
+                .IsUnicode(false)
+                .HasColumnName("DNI");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Asignatura>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Asignatu__3213E83FF11A84D6");
+
+            entity.ToTable("Asignatura");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Profesor)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.ProfesorNavigation).WithMany(p => p.Asignaturas)
+                .HasForeignKey(d => d.Profesor)
+                .HasConstraintName("FK__Asignatur__Profe__4E88ABD4");
+        });
+
+        modelBuilder.Entity<Calificacion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Califica__3213E83FEE1BFCF7");
+
+            entity.ToTable("Calificacion");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Matricula).WithMany(p => p.Calificacions)
+                .HasForeignKey(d => d.MatriculaId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Calificac__Matri__5535A963");
+        });
+
+        modelBuilder.Entity<Matricula>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Matricul__3213E83FE43A20C9");
+
+            entity.ToTable("Matricula");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+
+            entity.HasOne(d => d.Alumno).WithMany(p => p.Matriculas)
+                .HasForeignKey(d => d.AlumnoId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Matricula__Alumn__5165187F");
+
+            entity.HasOne(d => d.AsignaturaNavigation).WithMany(p => p.Matriculas)
+                .HasForeignKey(d => d.Asignatura)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Matricula__Asign__52593CB8");
+        });
+
+        modelBuilder.Entity<Profesor>(entity =>
+        {
+            entity.HasKey(e => e.Usuario).HasName("PK__Profesor__E3237CF67703CD4C");
+
+            entity.ToTable("Profesor");
+
+            entity.Property(e => e.Usuario)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Pass)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
