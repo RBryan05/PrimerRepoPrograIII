6ce4ab7701eb582e37b987b3d55a41ef90ce6d33
RBryan05 <rg0358032023@unab.edu.sv>
2025-03-01 17:07:25 -0600

    Guía 13: Crear Calificaciones

--- WebAPI/Controllers/CalificacionesController.cs
+++ WebAPI/Controllers/CalificacionesController.cs
@@ -11,10 +11,22 @@ namespace WebAPI.Controllers
     {
         private CalificacionDAO _cali = new CalificacionDAO();
 
+        #region GetCalificaciones
         [HttpGet("calificaciones")]
         public List<Calificacion> GetCalificaciones(int idMatricula)
         {
             return _cali.Seleccion(idMatricula);
         }
+        #endregion
+
+        #region PostCalificaciones
+        [HttpPost("calificaciones")]
+        public bool Insertar([FromBody] Calificacion calificacion)
+        {
+            return _cali.InsertCalificacion(calificacion);
+        }
+        #endregion
+
+
     }
 }
