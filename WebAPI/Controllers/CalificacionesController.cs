c13228beb126c981409a98ade270e52a9c94a082
RBryan05 <rg0358032023@unab.edu.sv>
2025-03-01 16:58:12 -0600

    Guía 12: EndPoint para calificaciones

---
+++ WebAPI/Controllers/CalificacionesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reactBackend.Models;
+using reactBackend.Repository;
+
+namespace WebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CalificacionesController : ControllerBase
+    {
+        private CalificacionDAO _cali = new CalificacionDAO();
+
+        [HttpGet("calificaciones")]
+        public List<Calificacion> GetCalificaciones(int idMatricula)
+        {
+            return _cali.Seleccion(idMatricula);
+        }
+    }
+}
