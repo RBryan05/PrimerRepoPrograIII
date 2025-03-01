171573d71bfb63fcd7e61d642be3fdb54ea51c0c
RBryan05 <rg0358032023@unab.edu.sv>
2025-03-01 09:20:08 -0600

    Guía 3: Inicio del CRUD

---
+++ reactBackend/Repository/AlumnoDAO.cs
@@ -0,0 +1,33 @@
+﻿using reactBackend.Context;
+using reactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactBackend.Repository
+{
+    public class AlumnoDAO
+    {
+        #region Context
+        public RegistroAlumnosContext contecxto = new RegistroAlumnosContext();
+        #endregion
+
+        #region SelectAll
+        public List<Alumno> SelectAll()
+        {
+            var alumno = contecxto.Alumnos.ToList<Alumno>();
+            return alumno;
+        }
+        #endregion
+
+        #region GetByID
+        public Alumno GetByID(int id)
+        {
+            var alumno = contecxto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            return alumno == null ? null : alumno;
+        }
+        #endregion
+    }
+}
